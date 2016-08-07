d7d4909e133129612afdb313208c4dbb750a0078
freek <freek@spatie.be>
2016-08-07 12:23:11 +0200

    wip

--- tests/DdTest.php
+++ tests/DdTest.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Collection;
 class DdTest extends TestCase
 {
     /** @test */
-    public function it_has_a_dd_macro()
+    public function it_provides_a_dd_macro()
     {
         $this->assertTrue(Collection::hasMacro('dd'));
     }
