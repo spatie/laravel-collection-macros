8148bc9cc68d79cd75437002c33600ad7b289ec9
Thom Hurks <thomhurks@gmail.com>
2017-03-20 22:49:46 +0100

    Group collections on multiple fields.
    - groupBy() can group collections on a single field, but
      groupByMultiple() can group collections on multiple fields and
      create a nested structure.
    - Heavily based on and partially backwards compatible with the
      existing groupBy(), so easy to switch to this function.

--- src/macros.php
+++ src/macros.php
@@ -297,3 +297,69 @@ if (! Collection::hasMacro('before')) {
         return $this->reverse()->after($currentItem, $fallback);
     });
 }
+
+if (! Collection::hasMacro('groupByMultiple')) {
+    /**
+     * Group an associative array by multiple fields or callbacks.
+     *
+     * @param  string|array  $groupBy
+     * @param  bool  $preserveKeys
+     * @return Collection
+     */
+    Collection::macro('groupByMultiple', function ($groupBy, $preserveKeys = false): Collection {
+        if (! is_array($groupBy)) {
+            $groupBy = [$groupBy];
+        }
+
+        $groupKeyRetrievers = [];
+        foreach ($groupBy as $currentGroupBy) {
+            $groupKeyRetrievers[] = $this->valueRetriever($currentGroupBy);
+        }
+
+        $results = [];
+
+        foreach ($this->items as $key => $value) {
+            $currentLevel = [&$results];
+            $nextLevel = [];
+            foreach ($groupKeyRetrievers as $currentGroupBy) {
+                $groupKeys = $currentGroupBy($value);
+                if (! is_array($groupKeys)) {
+                    $groupKeys = [$groupKeys];
+                }
+                foreach ($groupKeys as $groupKey) {
+                    foreach ($currentLevel as &$subGroup) {
+                        if (! array_key_exists($groupKey, $subGroup)) {
+                            $subGroup[$groupKey] = [];
+                        }
+                        $nextLevel[] = &$subGroup[$groupKey];
+                    }
+                }
+                $currentLevel = $nextLevel;
+                $nextLevel = [];
+            }
+            if ($preserveKeys && ! is_null($key)) {
+                foreach ($currentLevel as &$lastLevel) {
+                    $lastLevel[$key] = $value;
+                }
+            } else {
+                foreach ($currentLevel as &$lastLevel) {
+                    $lastLevel[] = $value;
+                }
+            }
+        }
+
+        $toNestedCollection = function(&$array) use (&$toNestedCollection) {
+            if (is_array($array)) {
+                foreach ($array as &$subArray) {
+                    $subArray = $toNestedCollection($subArray);
+                }
+
+                return new Collection($array);
+            } else {
+                return $array;
+            }
+        };
+
+        return $toNestedCollection($results);
+    });
+}
