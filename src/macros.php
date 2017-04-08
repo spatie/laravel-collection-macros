8132465c2a12e8e173d637887e7d6360178534b0
Dmytro <mcklayin@gmail.com>
2017-04-08 15:50:26 +0300

    reformat code

--- src/macros.php
+++ src/macros.php
@@ -315,6 +315,7 @@ if (! Collection::hasMacro('exists')) {
                 } else {
                     if (is_array($value)) {
                         $path[] = $key;
+
                         return $inArrayRecursive($needle, $value, $strict, $path);
                     }
                 }
