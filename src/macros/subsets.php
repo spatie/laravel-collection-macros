f3f1acae07dff4aed9a602adfc90ea22f5c067b0
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-09-08 12:27:49 -0400

    Create subsets.php

---
+++ src/macros/subsets.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+Collection::macro('subsets', function () {
+    $keys = array_keys($this->items);
+    $size = pow(2, $this->count());
+
+    $subsets = new Collection;
+
+    for ($i = 0; $i < $size; $i++) {
+        $subset = new static;
+        $code = $i ^ ($i >> 1);
+
+        for ($j = 0; $j < $this->count(); $j++) {
+            if (($code >> $j) & 1) {
+                $subset[$keys[$j]] = $this->items[$keys[$j]];
+            }
+        }
+
+        $subsets->push($subset);
+    }
+
+    return $subsets;
+});
