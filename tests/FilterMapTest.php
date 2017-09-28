1534f8fa0553926ff9158b0a81e15cf101da87c4
Freek Van der Herten <freek@spatie.be>
2017-09-28 14:34:59 +0000

    Apply fixes from StyleCI

--- tests/FilterMapTest.php
+++ tests/FilterMapTest.php
@@ -12,7 +12,7 @@ class FilterMapTest extends TestCase
         $result = Collection::make([1, 2, 3, 4, 5, 6])->filterMap(function ($number) {
             $quotient = $number / 3;
 
-            return is_integer($quotient) ? $quotient : null;
+            return is_int($quotient) ? $quotient : null;
         });
 
         $this->assertEquals([1, 2], $result->values()->toArray());
