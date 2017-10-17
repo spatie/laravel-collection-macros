684c4276572e7200d9d1c9e72d013bdd40c0bb92
Alexander Lichter <manniL@gmx.net>
2017-10-17 18:20:31 +0200

    Add byIndex macro (#87)
    * Add byIndex macro
    
    * Rename byIndex to at

---
+++ src/macros/at.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+/**
+ * Get a single item from the collection by index.
+ *
+ * @param mixed $index
+ *
+ * @return mixed
+ */
+Collection::macro('at', function ($index) {
+    return $this->slice($index, 1)->first();
+});
---
+++ tests/AtTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spatie\CollectionMacros\Test;
+
+use Illuminate\Support\Collection;
+
+class AtTest extends TestCase
+{
+    /** @test */
+    public function it_retrieves_an_item_by_positive_index()
+    {
+        $data = new Collection([1, 2, 3]);
+
+        $this->assertEquals(2, $data->at(1));
+    }
+
+    /** @test */
+    public function it_retrieves_an_item_by_negative_index()
+    {
+        $data = new Collection([1, 2, 3]);
+
+        $this->assertEquals(3, $data->at(-1));
+    }
+
+    /** @test */
+    public function it_retrieves_an_item_by_zero_index()
+    {
+        $data = new Collection([1, 2, 3]);
+
+        $this->assertEquals(1, $data->at(0));
+    }
+}
