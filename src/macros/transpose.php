bedf72f3355c8e1564bf9ed84adde986f96e269f
Mateusz Drost <mat.drost@gmail.com>
2017-10-29 11:58:11 +0100

    Allow to transpose arrayable (#91)
    * Allow to transpose empty collection.
    
    * Allow to transpose arrayable.

--- src/macros/transpose.php
+++ src/macros/transpose.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Collection;
  * @throws \LengthException
  */
 Collection::macro('transpose', function (): Collection {
+    if ($this->isEmpty()) {
+        return new static();
+    }
+
     $expectedLength = count($this->first());
 
     array_walk($this->items, function ($row) use ($expectedLength) {
@@ -20,7 +24,9 @@ Collection::macro('transpose', function (): Collection {
 
     $items = array_map(function (...$items) {
         return new static($items);
-    }, ...$this->values());
+    }, ...array_map(function ($items) {
+        return $this->getArrayableItems($items);
+    }, array_values($this->items)));
 
     return new static($items);
 });
