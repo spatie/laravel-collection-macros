1d63471e9c4d5f1227c4cd0aaac3fe86dfb0da02
Gijs Jorissen <gizburdt@users.noreply.github.com>
2017-11-13 13:26:26 +0100

    FirstOrFail (#93)
    * FirstOrFail
    
    * CollectionItemNotFound exception

---
+++ src/macros/firstOrFail.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Spatie\CollectionMacros\Exceptions\CollectionItemNotFound;
+
+/*
+ * Get the first item. Throws Exception when collection is empty.
+ *
+ * @return mixed
+ */
+Collection::macro('firstOrFail', function () {
+    if (! is_null($item = $this->first())) {
+        return $item;
+    }
+
+    throw new CollectionItemNotFound('No items found in collection.');
+});
