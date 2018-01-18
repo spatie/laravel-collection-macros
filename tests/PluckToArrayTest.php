8a18858817e093e3c5e2de8e2330062df14267ad
Andriy Sushchyk <andrii.sushchyk@gmail.com>
2018-01-19 00:56:05 +0200

    Add pluckToArray method (#111)

---
+++ tests/PluckToArrayTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Spatie\CollectionMacros\Test;
+
+use Illuminate\Support\Collection;
+
+class PluckToArrayTest extends TestCase
+{
+    /** @test */
+    public function it_returns_array_of_attributes()
+    {
+        $result = Collection::make([
+            ['id' => 1],
+            ['id' => 2],
+            ['id' => 3],
+        ])->pluckToArray('id');
+
+        $expected = [1, 2, 3];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function it_return_array_of_attributes_with_correct_keys()
+    {
+        $result = Collection::make([
+           ['id' => 2, 'title' => 'A'],
+           ['id' => 3, 'title' => 'B'],
+           ['id' => 4, 'title' => 'C'],
+        ])->pluckToArray('title', 'id');
+
+        $expected = [2 => 'A', 3 => 'B', 4 => 'C'];
+
+        $this->assertEquals($expected, $result);
+    }
+}
