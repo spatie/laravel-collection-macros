cff3f0b1ffe0322983a2e4c2306a38b381382c08
freek <freek@spatie.be>
2018-12-07 16:32:35 +0100

    add test

--- tests/Macros/PrioritizeTest.php
+++ tests/Macros/PrioritizeTest.php
@@ -8,7 +8,7 @@ use Spatie\CollectionMacros\Test\TestCase;
 class PrioritizeTest extends TestCase
 {
     /** @test */
-    public function it_moves_elements_to_the_start_of_the_collection()
+    public function it_moves_a_single_element_to_the_start_of_the_collection()
     {
         $collection = Collection::make([
             ['id' => 1],
@@ -21,5 +21,22 @@ class PrioritizeTest extends TestCase
         });
 
         $this->assertEquals([2,1,3], $prioritized->pluck('id')->toArray());
+    }
+
+    /** @test */
+    public function it_moves_multiple_elements_to_the_start_of_the_collection()
+    {
+        $collection = Collection::make([
+            ['id' => 1],
+            ['id' => 2],
+            ['id' => 3],
+            ['id' => 4],
+        ]);
+
+        $prioritized = $collection->prioritize(function(array $item) {
+            return in_array($item['id'], [2,4]);
+        });
+
+        $this->assertEquals([2,4,1,3], $prioritized->pluck('id')->toArray());
     }
 }
