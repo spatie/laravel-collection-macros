23441ea7bde0c6192963f55b50080a15f7f56f04
chapeupreto <rodiney@gmail.com>
2018-12-14 21:37:24 -0200

    add head macro

---
+++ src/macros/head.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+/*
+ * Get the first item from the collection.
+ *
+ * @return mixed
+ */
+Collection::macro('head', function() {
+    return $this->first();
+});
