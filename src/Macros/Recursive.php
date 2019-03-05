d61fce8db01b9b2539cc5cd2fcced50dd372f772
Tarek Adam <tarek.adam@gmail.com>
2019-03-05 09:21:59 -0600

    adds macro to collect recursively

---
+++ src/Macros/Recursive.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spatie\CollectionMacros\Macros;
+
+/**
+ * Use to deep collect multi-dimensional associative arrays.
+ */
+class Recursive
+{
+	public function __invoke(){
+		return function () {
+			return $this->map(function ($value) {
+				if (is_array($value) || is_object($value)) {
+					return collect($value)->recursive();
+				}
+
+				return $value;
+			});
+		};
+	}
+}
