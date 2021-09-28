1c3e4efd1804d5f676a4bb99e5525fb564cba65c
Matthew Bowen <matthew.bowen1989@protonmail.com>
2021-09-28 15:18:53 +0900

    ✅ Add insertAt tests

--- tests/Macros/InsertAtTest.php
+++ tests/Macros/InsertAtTest.php
@@ -14,11 +14,24 @@ class InsertAtTest extends TestCase
     }
 
     /** @test */
-    public function it_inserts_at_correct_index()
+    public function it_inserts_at_the_correct_index()
     {
-        $data = Collection::make(['zero', 'two', 'three']);
-        $data->insertAt(1, 'one');
+        $collection = Collection::make(['zero', 'two', 'three']);
+        $collection->insertAt(1, 'one');
+        $this->assertEquals(['zero', 'one', 'two', 'three'], $collection->toArray());
+    }
 
-        $this->assertEquals(['zero', 'one', 'two', 'three'], $data->toArray());
+    /** @test */
+    public function it_returns_the_updated_collection()
+    {
+        $collection = Collection::make(['zero', 'two', 'three'])->insertAt(1, 'one');
+        $this->assertEquals(['zero', 'one', 'two', 'three'], $collection->toArray());
+    }
+
+    /** @test */
+    public function it_maintains_array_keys()
+    {
+        $collection = Collection::make(['zero' => 0, 'two' => 2, 'three' => 3])->insertAt(1, 'one');
+        $this->assertEquals(['zero' => 0, 'one', 'two' => 2, 'three' => 3], $collection->toArray());
     }
 }
