66a5c03e02650d07dad9e9c3bfe7feddf256d3d3
Matthew Bowen <matthew.bowen1989@protonmail.com>
2021-09-28 18:02:55 +0900

    ✨ Add insert before/after macros

---
+++ src/Macros/InsertBefore.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spatie\CollectionMacros\Macros;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Inserts an item before another item.
+ *
+ * @param  mixed $before
+ * @param  mixed $item
+ * @param  mixed $key
+ *
+ * @mixin \Illuminate\Support\Collection
+ *
+ * @return Collection
+ */
+class InsertBefore
+{
+    public function __invoke()
+    {
+        return function ($before, $item, $key = null): Collection {
+            $beforeKey = array_search($before, $this->items);
+            return $this->insertBeforeKey($beforeKey, $item, $key);
+        };
+    }
+}
---
+++ src/Macros/InsertBeforeKey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spatie\CollectionMacros\Macros;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Inserts an item before another item by key.
+ *
+ * @param  mixed $beforeKey
+ * @param  mixed $item
+ * @param  mixed $key
+ *
+ * @mixin \Illuminate\Support\Collection
+ *
+ * @return Collection
+ */
+class InsertBeforeKey
+{
+    public function __invoke()
+    {
+        return function ($beforeKey, $item, $key = null): Collection {
+            $index = array_search($beforeKey, array_keys($this->items));
+            return $this->insertAt($index, $item, $key);
+        };
+    }
+}
