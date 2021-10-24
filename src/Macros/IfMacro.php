130a6e851fc2346070ee89b770a7f0ff4574c263
freek <freek@spatie.be>
2021-10-24 17:24:53 +0200

    wip

--- src/Macros/IfMacro.php
+++ src/Macros/IfMacro.php
@@ -6,14 +6,8 @@ class IfMacro
 {
     public function __invoke()
     {
-        return function (
-            mixed $if,
-            mixed $then = null,
-            mixed $else = null,
-        ): mixed {
-            return value($if, $this)
-                ? value($then, $this)
-                : value($else, $this);
+        return function (mixed $if, mixed $then = null, mixed $else = null): mixed {
+            return value($if, $this) ? value($then, $this) : value($else, $this);
         };
     }
 }
