381f439b5b6af24bd35d12fc0f970eacdf20bcc0
Alex Garrett-Smith <alex@codecourse.com>
2021-11-13 20:55:14 +0000

    Added recursive macro

---
+++ tests/Macros/RecursiveTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Spatie\CollectionMacros\Test\Macros;
+
+use Illuminate\Support\Collection;
+use Spatie\CollectionMacros\Test\TestCase;
+
+class RecursiveTest extends TestCase
+{
+    /** @test */
+    public function it_coverts_child_arrays_to_collections()
+    {
+        $collection = Collection::make([
+            'child' => [
+                1, 2, 3, 'anotherchild' => [1, 2, 3]
+            ],
+        ])
+            ->recursive();
+
+        $this->assertInstanceOf(Collection::class, $collection['child']);
+        $this->assertInstanceOf(Collection::class, $collection['child']['anotherchild']);
+    }
+
+    /** @test */
+    public function it_coverts_child_objects_to_collections()
+    {
+        $collection = Collection::make([
+            'child' => (object) [1, 2, 3, 'anotherchild' => (object) [1, 2, 3]],
+        ])
+            ->recursive();
+
+        $this->assertInstanceOf(Collection::class, $collection['child']);
+        $this->assertInstanceOf(Collection::class, $collection['child']['anotherchild']);
+    }
+}
