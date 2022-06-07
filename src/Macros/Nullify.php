c1c66a767582e8e81b9464b0e5fce8962a90c185
Michael Rubél <contact@observer.name>
2022-06-07 16:11:57 +0200

    add `nullify` macro class

---
+++ src/Macros/Nullify.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Spatie\CollectionMacros\Macros;
+
+use ArrayAccess;
+
+/**
+ * Checks whether a collection has blank
+ * values and sets the values null if so.
+ *
+ * @mixin \Illuminate\Support\Collection
+ *
+ * @return \Illuminate\Support\Collection
+ *
+ */
+class Nullify
+{
+    public function __invoke()
+    {
+        return function () {
+            return $this->map(
+                fn ($value) => ! blank($value)
+                    ? (function () use ($value) {
+                        if (is_array($value) || $value instanceof ArrayAccess) {
+                            foreach ($value as $key => $nestedValue) {
+                                if (blank($nestedValue)) {
+                                    $value[$key] = null;
+                                }
+                            }
+                        }
+
+                        return $value;
+                    })()
+                    : null
+            );
+        };
+    }
+}
