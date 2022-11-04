43524fc27dc725391cc0b782127ac4cbe9f216c3
Alex Manase <malexandru699@gmail.com>
2022-11-04 12:16:19 +0200

    refactor `AtTest`

--- tests/Macros/AtTest.php
+++ tests/Macros/AtTest.php
@@ -1,33 +1,21 @@
 <?php
 
-namespace Spatie\CollectionMacros\Test\Macros;
-
 use Illuminate\Support\Collection;
-use Spatie\CollectionMacros\Test\TestCase;
 
-class AtTest extends TestCase
-{
-    /** @test */
-    public function it_retrieves_an_item_by_positive_index()
-    {
-        $data = new Collection([1, 2, 3]);
+it('retrieves an item by positive index', function () {
+    $data = new Collection([1, 2, 3]);
 
-        $this->assertEquals(2, $data->at(1));
-    }
+    expect($data->at(1))->toEqual(2);
+});
 
-    /** @test */
-    public function it_retrieves_an_item_by_negative_index()
-    {
-        $data = new Collection([1, 2, 3]);
+it('retrieves an item by negative index', function () {
+    $data = new Collection([1, 2, 3]);
 
-        $this->assertEquals(3, $data->at(-1));
-    }
+    expect($data->at(-1))->toEqual(3);
+});
 
-    /** @test */
-    public function it_retrieves_an_item_by_zero_index()
-    {
-        $data = new Collection([1, 2, 3]);
+it('retrieves an item by zero index', function () {
+    $data = new Collection([1, 2, 3]);
 
-        $this->assertEquals(1, $data->at(0));
-    }
-}
+    expect($data->at(0))->toEqual(1);
+});
