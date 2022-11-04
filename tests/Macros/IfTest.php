9594de930c3b3bff6a619892e83e2f230b53f4c7
Alex Manase <malexandru699@gmail.com>
2022-11-04 13:57:19 +0200

    refactor `IfTest`

--- tests/Macros/IfTest.php
+++ tests/Macros/IfTest.php
@@ -1,54 +1,32 @@
 <?php
 
-namespace Spatie\CollectionMacros\Test\Macros;
-
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
-use Spatie\CollectionMacros\Test\TestCase;
 
-class IfTest extends TestCase
-{
-    /** @test */
-    public function it_will_return_the_right_branch()
-    {
-        $this->assertTrue(collect()->if(true, then: true, else: false));
-        $this->assertFalse(collect()->if(false, then: true, else: false));
-    }
+it('will return the right branch')
+    ->expect(fn () => collect()->if(true, then: true, else: false))->toBeTrue()
+    ->and(fn () => collect()->if(false, then: true, else: false))->toBeFalse();
 
-    /**
-     * @test
-     *
-     * @dataProvider sentences
-     */
-    public function it_will_pass_the_collection_to_the_branches(string $sentence, string $modifiedSentence)
-    {
-        $collection = collect([$sentence])
-            ->if(
-                fn (Collection $collection) => $collection->contains('this is the value'),
-                then: fn (Collection $collection) => $collection->map(fn (string $item) => strtoupper($item)),
-                else: fn (Collection $collection) => $collection->map(fn (string $item) => Str::kebab($item))
-            );
+it('will pass the collection to the branches', function (string $sentence, string $modifiedSentence) {
+    $collection = collect([$sentence])
+        ->if(
+            fn (Collection $collection) => $collection->contains('this is the value'),
+            then: fn (Collection $collection) => $collection->map(fn (string $item) => strtoupper($item)),
+            else: fn (Collection $collection) => $collection->map(fn (string $item) => Str::kebab($item))
+        );
 
-        $this->assertEquals($modifiedSentence, $collection[0]);
-    }
+    expect($collection[0])->toEqual($modifiedSentence);
+})->with([
+    ['this is the value', 'THIS IS THE VALUE'],
+    ['this is another value', 'this-is-another-value'],
+]);
 
-    public function sentences(): array
-    {
-        return [
-            ['this is the value', 'THIS IS THE VALUE'],
-            ['this is another value', 'this-is-another-value'],
-        ];
-    }
+test('the branches are optional', function () {
+    $result = collect(['this is a value'])
+        ->if(
+            false,
+            then: fn (Collection $collection) => 'something',
+        );
 
-    /** @test */
-    public function the_branches_are_optional()
-    {
-        $result = collect(['this is a value'])
-            ->if(
-                false,
-                then: fn (Collection $collection) => 'something',
-            );
-
-        $this->assertNull($result);
-    }
-}
+    expect($result)->toBeNull();
+});
