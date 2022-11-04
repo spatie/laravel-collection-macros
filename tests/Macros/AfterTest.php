f83e0c3039618ea4746c844aa882d8b8d7700288
Alex Manase <malexandru699@gmail.com>
2022-11-04 12:14:00 +0200

    refactor `AfterTest`

--- tests/Macros/AfterTest.php
+++ tests/Macros/AfterTest.php
@@ -1,69 +1,51 @@
 <?php
 
-namespace Spatie\CollectionMacros\Test\Macros;
-
 use Illuminate\Support\Collection;
-use Spatie\CollectionMacros\Test\TestCase;
 
-class AfterTest extends TestCase
-{
-    /** @test */
-    public function it_can_retrieve_an_item_that_comes_after_an_item()
-    {
-        $data = new Collection([1, 2, 3]);
+it('can retrieve an item that comes after an item', function () {
+    $data = new Collection([1, 2, 3]);
 
-        $this->assertEquals(2, $data->after(1));
-    }
+    expect($data->after(1))->toEqual(2);
+});
 
-    /** @test */
-    public function it_retrieves_items_by_value_and_doesnt_reorder_them()
-    {
-        $data = new Collection([
-            4 => 3,
-            2 => 1,
-            1 => 2,
-            3 => 4,
-        ]);
+it('retrieves items by value and does not reorder them', function () {
+    $data = new Collection([
+        4 => 3,
+        2 => 1,
+        1 => 2,
+        3 => 4,
+    ]);
 
-        $this->assertEquals(1, $data->after(3));
-    }
+    expect($data->after(3))->toEqual(1);
+});
 
-    /** @test */
-    public function it_can_find_the_next_item_in_a_collection_of_strings()
-    {
-        $data = new Collection([
-            'foo' => 'bar',
-            'bar' => 'foo',
-        ]);
+it('can fix the next item in a collection of strings', function () {
+    $data = new Collection([
+        'foo' => 'bar',
+        'bar' => 'foo',
+    ]);
 
-        $this->assertEquals('foo', $data->after('bar'));
-    }
+    expect($data->after('bar'))->toEqual('foo');
+});
 
-    /** @test */
-    public function it_can_find_the_next_item_based_on_a_callback()
-    {
-        $data = new Collection([3, 1, 2]);
+it('can find the next item based on a callback', function () {
+    $data = new Collection([3, 1, 2]);
 
-        $result = $data->after(function ($item) {
-            return $item > 2;
-        });
+    $result = $data->after(function ($item) {
+        return $item > 2;
+    });
 
-        $this->assertEquals(1, $result);
-    }
+    expect($result)->toEqual(1);
+});
 
-    /** @test */
-    public function it_returns_null_if_there_isnt_a_next_item()
-    {
-        $data = new Collection([1, 2, 3]);
+it('returns `null` if there is not a next item', function () {
+    $data = new Collection([1, 2, 3]);
 
-        $this->assertNull($data->after(3));
-    }
+    expect($data->after(3))->toBeNull();
+});
 
-    /** @test */
-    public function it_can_return_a_fallback_value_if_there_isnt_a_next_item()
-    {
-        $data = new Collection([1, 2, 3]);
+it('can return a fallback value if there is not a next item', function () {
+    $data = new Collection([1, 2, 3]);
 
-        $this->assertEquals(4, $data->after(3, 4));
-    }
-}
+    expect($data->after(3, 4))->toEqual(4);
+});
