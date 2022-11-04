9a97897c2ac979254a0ffae9043b8ab7fc169b3c
Alex Manase <malexandru699@gmail.com>
2022-11-04 14:10:21 +0200

    refactor `InsertAtTest`

--- tests/Macros/InsertAtTest.php
+++ tests/Macros/InsertAtTest.php
@@ -1,51 +1,36 @@
 <?php
 
-namespace Spatie\CollectionMacros\Test\Macros;
-
 use Illuminate\Support\Collection;
-use Spatie\CollectionMacros\Test\TestCase;
 
-class InsertAtTest extends TestCase
-{
-    /** @test */
-    public function it_provides_an_insertAt_macro()
-    {
-        $this->assertTrue(Collection::hasMacro('insertAt'));
-    }
+it('provides an `insertAt` macro')
+    ->expect(fn () => Collection::hasMacro('insertAt'))
+    ->toBeTrue();
 
-    /** @test */
-    public function it_inserts_at_the_correct_index()
-    {
-        $collection = Collection::make(['zero', 'two', 'three']);
-        $collection->insertAt(1, 'one');
-        $this->assertEquals(json_encode(['zero', 'one', 'two', 'three']), json_encode($collection->toArray()));
-    }
+it('inserts at the correct index', function () {
+    $collection = Collection::make(['zero', 'two', 'three']);
+    $collection->insertAt(1, 'one');
 
-    /** @test */
-    public function it_returns_the_updated_collection()
-    {
-        $collection = Collection::make(['zero', 'two', 'three'])->insertAt(1, 'one');
-        $this->assertEquals(json_encode(['zero', 'one', 'two', 'three']), json_encode($collection->toArray()));
-    }
+    expect(json_encode($collection->toArray()))
+        ->toEqual(json_encode(['zero', 'one', 'two', 'three']));
+});
 
-    /** @test */
-    public function it_maintains_array_keys()
-    {
-        $collection = Collection::make(['zero' => 0, 'two' => 2, 'three' => 3])->insertAt(1, 'one');
-        $this->assertEquals(
-            json_encode(['zero' => 0, 'one', 'two' => 2, 'three' => 3]),
-            json_encode($collection->toArray())
-        );
-    }
+it('returns the updated collection', function () {
+    $collection = Collection::make(['zero', 'two', 'three'])->insertAt(1, 'one');
 
-    /** @test */
-    public function it_inserts_with_a_key()
-    {
-        $collection = Collection::make(['zero' => 0, 'two' => 2, 'three' => 3])->insertAt(1, 5, 'five');
+    expect(json_encode($collection->toArray()))
+        ->toEqual(json_encode(['zero', 'one', 'two', 'three']));
+});
 
-        $this->assertEquals(
-            json_encode(['zero' => 0, 'five' => 5, 'two' => 2, 'three' => 3]),
-            json_encode($collection->toArray())
-        );
-    }
-}
+it('maintains array keys', function () {
+    $collection = Collection::make(['zero' => 0, 'two' => 2, 'three' => 3])->insertAt(1, 'one');
+
+    expect(json_encode($collection->toArray()))
+        ->toEqual(json_encode(['zero' => 0, 'one', 'two' => 2, 'three' => 3]));
+});
+
+it('inserts with a key', function () {
+    $collection = Collection::make(['zero' => 0, 'two' => 2, 'three' => 3])->insertAt(1, 5, 'five');
+
+    expect(json_encode($collection->toArray()))
+        ->toEqual(json_encode(['zero' => 0, 'five' => 5, 'two' => 2, 'three' => 3]));
+});
