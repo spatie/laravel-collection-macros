b9ec5ae97fda942a273f1f50ee3fc3b688cafb1d
Alex Manase <malexandru699@gmail.com>
2022-11-04 15:18:46 +0200

    refactor `NoneTest`

--- tests/Macros/NoneTest.php
+++ tests/Macros/NoneTest.php
@@ -1,52 +1,44 @@
 <?php
 
-namespace Spatie\CollectionMacros\Test\Macros;
-
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Collection;
-use Spatie\CollectionMacros\Test\TestCase;
 
-class NoneTest extends TestCase
-{
-    /** @test */
-    public function it_can_check_if_an_item_isnt_present_in_a_collection()
-    {
-        $this->assertTrue(Collection::make(['foo'])->none('bar'));
-        $this->assertFalse(Collection::make(['foo'])->none('foo'));
+it("can check if an item isn't present in a collection")
+    ->expect(fn () => Collection::make(['foo'])->none('bar'))->toBeTrue()
+    ->and(fn () => Collection::make(['foo'])->none('foo'))->toBeFalse();
+
+it("can check if a key-value pair isn't present in a collection")
+    ->expect(fn () => Collection::make([['name' => 'foo']])->none('name', 'bar'))
+    ->toBeTrue()
+    ->and(fn () => Collection::make([['name' => 'foo']])->none('name', 'foo'))
+    ->toBeFalse();
+
+it("can check if something isn't present in a collection with truth test", function () {
+    // Below Laravel 5.3, the callable's parameter order is `$key, $value`.
+
+    if (version_compare(Application::VERSION, '5.3.0', 'lt')) {
+        expect(
+            Collection::make(['name' => 'foo'])->none(
+                fn ($key, $value) => $key === 'name' && $value === 'bar'
+            )
+        )->toBeTrue();
+
+        expect(
+            Collection::make(['name' => 'foo'])->none(
+                fn ($key, $value) => $key === 'name' && $value === 'foo'
+            )
+        )->toBeFalse();
+
+        return;
     }
 
-    /** @test */
-    public function it_can_check_if_a_key_value_pair_isnt_present_in_a_collection()
-    {
-        $this->assertTrue(Collection::make([['name' => 'foo']])->none('name', 'bar'));
-        $this->assertFalse(Collection::make([['name' => 'foo']])->none('name', 'foo'));
-    }
+    // Above Laravel 5.3, the callable's parameter order is `$value, $key`.
 
-    /** @test */
-    public function it_can_check_if_something_isnt_present_in_a_collection_with_a_truth_test()
-    {
-        // Below Laravel 5.3, the callable's parameter order is `$key, $value`.
+    expect(Collection::make(['name' => 'foo'])->none(
+        fn ($value, $key) => $key === 'name' && $value === 'bar'
+    ))->toBeTrue();
 
-        if (version_compare(Application::VERSION, '5.3.0', 'lt')) {
-            $this->assertTrue(Collection::make(['name' => 'foo'])->none(function ($key, $value) {
-                return $key === 'name' && $value === 'bar';
-            }));
-
-            $this->assertFalse(Collection::make(['name' => 'foo'])->none(function ($key, $value) {
-                return $key === 'name' && $value === 'foo';
-            }));
-
-            return;
-        }
-
-        // Above Laravel 5.3, the callable's parameter order is `$value, $key`.
-
-        $this->assertTrue(Collection::make(['name' => 'foo'])->none(function ($value, $key) {
-            return $key === 'name' && $value === 'bar';
-        }));
-
-        $this->assertFalse(Collection::make(['name' => 'foo'])->none(function ($value, $key) {
-            return $key === 'name' && $value === 'foo';
-        }));
-    }
-}
+    expect(Collection::make(['name' => 'foo'])->none(
+        fn ($value, $key) => $key === 'name' && $value === 'foo'
+    ))->toBeFalse();
+});
