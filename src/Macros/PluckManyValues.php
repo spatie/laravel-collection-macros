2c733a621adc1f9663dad42b284cc1cf94fece98
sfinktah <sfinktah@github.spamtrak.org>
2023-04-07 06:32:24 +1000

    added pluckManyValues; extended pluckMany

---
+++ src/Macros/PluckManyValues.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Spatie\CollectionMacros\Macros;
+
+use ArrayAccess;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+
+/**
+ * Get a Collection with only the specified keys.
+ *
+ * @param  array  $keys
+ *
+ * @mixin \Illuminate\Support\Collection
+ *
+ * @return Collection
+ */
+class PluckManyValues
+{
+    public function __invoke()
+    {
+        return function ($keys): Collection {
+            // Allow passing multiple keys as multiple arguments
+            $keys = is_array($keys) ? $keys : func_get_args();
+            return $this->pluckMany($keys)->map(function ($item) {
+                if ($item instanceof Collection) {
+                    return $item->values();
+                }
+
+                if (is_array($item)) {
+                    return array_values($item);
+                }
+
+                // if ($item instanceof ArrayAccess) {
+                //     return collect($keys)->mapWithKeys(function ($key) use ($item) {
+                //         return [$key => $item[$key]];
+                //     })->toArray();
+                // }
+
+                return (object) array_values(get_object_vars($item));
+            });
+        };
+    }
+}
